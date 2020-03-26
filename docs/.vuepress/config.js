c7210919fe9a78c890367b4e43ebb945ecce884f
Tobias Thaden <hello@tobiasthaden.com>
2020-03-26 20:50:47 +0100

    Docs settings

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: "Cinema",
+
+    themeConfig: {
+        logo: null,
+
+        nav: [
+            { text: "GitHub", link: "https://github.com/tobiasthaden/cinema" },
+        ],
+
+        displayAllHeaders: true,
+    },
+};
